926a978817e5fbbb6d3fa0fc4fdc382fb92da6fa
bilikhasan <fatherlyy@hotmail.com.tr>
2024-10-10 23:46:27 +0300

    Bölüm 6: Ürünler bitti.

--- MvcOnlineTicariOtomasyon/Controllers/KategoriController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/KategoriController.cs
@@ -28,5 +28,25 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             c.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult KategoriSil(int id)
+        {
+            var ktg = c.Kategoris.Find(id);
+            c.Kategoris.Remove(ktg);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KategoriGetir(int id)
+        {
+            var kategori = c.Kategoris.Find(id);
+            return View("KategoriGetir", kategori);
+        }
+        public ActionResult KategoriGuncelle (Kategori k )
+        {
+            var ktgr = c.Kategoris.Find(k.KategoriID);
+            ktgr.KategoriAd = k.KategoriAd;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
