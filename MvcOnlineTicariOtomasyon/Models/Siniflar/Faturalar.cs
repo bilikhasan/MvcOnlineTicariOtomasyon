ea52c899b6fa34a63153636d7ff034257b7704f7
bilikhasan <fatherlyy@hotmail.com.tr>
2024-10-19 17:30:33 +0300

    77.bitti.

--- MvcOnlineTicariOtomasyon/Models/Siniflar/Faturalar.cs
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Faturalar.cs
@@ -32,7 +32,11 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
         [Column(TypeName = "Varchar")]
         [StringLength(60)]
         public string VergiDairesi { get; set; }
-        public DateTime Saat { get; set; }
+
+
+        [Column(TypeName = "Char")]
+        [StringLength(5)]
+        public string Saat { get; set; }
 
 
 
@@ -46,6 +50,10 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
         public string TeslimAlan { get; set; }
+
+
+        public decimal Toplam { get; set; }
+
 
 
         public ICollection<FaturaKalem> FaturaKalems { get; set; }
