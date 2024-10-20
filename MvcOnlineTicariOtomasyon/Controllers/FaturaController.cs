e73da9718b4a025c4c8ed18b02c613716a641e1a
bilikhasan <fatherlyy@hotmail.com.tr>
2024-10-20 23:36:26 +0300

    83.bitti.

--- MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -27,6 +28,44 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         public ActionResult FaturaEkle(Faturalar f)
         {
             c.Faturalars.Add(f);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult FaturaGetir(int id)
+        {
+            var fatura = c.Faturalars.Find(id);
+            return View("FaturaGetir", fatura);
+        }
+        public ActionResult FaturaGuncelle(Faturalar f)
+        {
+            var fatura = c.Faturalars.Find(f.Faturaid);
+            fatura.FaturaSeriNo = f.FaturaSeriNo;
+            fatura.FaturaSiraNo = f.FaturaSiraNo;
+            fatura.Saat = f.Saat;
+            fatura.Tarih = f.Tarih;
+            fatura.TeslimEden = f.TeslimEden;
+            fatura.TeslimAlan = f.TeslimAlan;
+            fatura.VergiDairesi = f.VergiDairesi;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult FaturaDetay(int id)
+        {
+            var degerler = c.FaturaKalems.Where(x => x.Faturaid == id).ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniKalem()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniKalem(FaturaKalem p)
+        {
+            c.FaturaKalems.Add(p);
             c.SaveChanges();
             return RedirectToAction("Index");
         }
