a2f92d856a6afbdc7683503e9b4c6353da0cd8c1
bilikhasan <fatherlyy@hotmail.com.tr>
2024-11-08 19:24:13 +0300

    176.bitti.

---
+++ MvcOnlineTicariOtomasyon/Migrations/202411081605096_denemekargo2.cs
@@ -0,0 +1,20 @@
+﻿namespace MvcOnlineTicariOtomasyon.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class denemekargo2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.KargoDetays", "Tarih", c => c.DateTime(nullable: false));
+            DropColumn("dbo.KargoDetays", "Date");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.KargoDetays", "Date", c => c.DateTime(nullable: false));
+            DropColumn("dbo.KargoDetays", "Tarih");
+        }
+    }
+}
