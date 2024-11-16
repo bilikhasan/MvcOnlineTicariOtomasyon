71dd3ffb2c42024a923175b21d27f373205aa767
bilikhasan <fatherlyy@hotmail.com.tr>
2024-11-16 16:51:50 +0300

    179.bitti.

--- MvcOnlineTicariOtomasyon/Controllers/KargoController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/KargoController.cs
@@ -12,10 +12,41 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         // GET: Kargo
         Context c = new Context();
 
-        public ActionResult Index()
+        public ActionResult Index(string p)
         {
-            var kargolar = c.KargoDetays.ToList();
-            return View(kargolar);
+            var k = from x in c.KargoDetays select x;
+            if (!string.IsNullOrEmpty(p))
+            {
+                k = k.Where(y => y.TakipKodu.Contains(p));
+            }
+            return View(k.ToList());
+
+        }
+
+        [HttpGet]
+        public ActionResult YeniKargo()
+        {
+            Random rnd = new Random();
+            string[] karakter = { "A", "B", "C", "D", "E", "F", "G", "H", "K" };
+            int k1, k2, k3;
+            k1 = rnd.Next(0, karakter.Length);
+            k2 = rnd.Next(0, karakter.Length);
+            k3 = rnd.Next(0, karakter.Length);
+            int s1, s2, s3;
+            s1 = rnd.Next(100, 1000);  //>>> 3 1 2 1 2 1  
+            s2 = rnd.Next(10, 99);
+            s3 = rnd.Next(10, 99);
+            string kod = s1.ToString() + karakter[k1] + s2 + karakter[k2] + s3 + karakter[k3];
+            ViewBag.takipkod = kod;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniKargo(KargoDetay d)
+        {
+            c.KargoDetays.Add(d);
+            c.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
