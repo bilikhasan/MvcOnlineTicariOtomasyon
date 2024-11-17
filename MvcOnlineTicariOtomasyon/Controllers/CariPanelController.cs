ed8baad3a39c629a9e16841163a5187e7617d516
bilikhasan <fatherlyy@hotmail.com.tr>
2024-11-17 23:07:21 +0300

    212.bitti.

--- MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -36,9 +36,37 @@ namespace MvcOnlineTicariOtomasyon.Controllers
 
         public ActionResult GelenMesajlar()
         {
-            var mesaj = c.Mesajlars.ToList();
-            return View(mesaj);
+            var mail = (string)Session["CariMail"];
+            var mesajlar = c.Mesajlars.Where(x=>x.Alici==mail).ToList();
+            var gelensayisi = c.Mesajlars.Count(x => x.Alici == mail).ToString();
+            ViewBag.d1 = gelensayisi;
+            var gidensayisi = c.Mesajlars.Count(x => x.Gonderici == mail).ToString();
+            ViewBag.d2 = gidensayisi;
+            return View(mesajlar);
         }
+
+        public ActionResult GidenMesajlar()
+        {
+            var mail = (string)Session["CariMail"];
+            var mesajlar = c.Mesajlars.Where(x => x.Gonderici == mail).ToList();
+            var gelensayisi = c.Mesajlars.Count(x => x.Alici == mail).ToString();
+            ViewBag.d1 = gelensayisi;
+            var gidensayisi = c.Mesajlars.Count(x => x.Gonderici == mail).ToString();
+            ViewBag.d2 = gidensayisi;
+            return View(mesajlar);
+        }
+
+        public ActionResult MesajDetay()
+        {
+            var gelensayisi = c.Mesajlars.Count(x => x.Alici == mail).ToString();
+            ViewBag.d1 = gelensayisi;
+            var gidensayisi = c.Mesajlars.Count(x => x.Gonderici == mail).ToString();
+            ViewBag.d2 = gidensayisi;
+            return View();
+        }
+
+
+
 
         //[HttpGet]
         //public ActionResult YeniMesaj()
